2a41afa133a99c085b27465cecd0d9512f02a043
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-08-13 17:51:01 +0530

    Adding Links to Drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+    private statusBar: StatusBar) {
     this.initializeApp();
   }
 
@@ -23,5 +22,8 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogout() {
   }
 }
